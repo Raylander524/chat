a01dfaa9ac37e6bf24251664af55d0bcd3fb04f7
Raylander524 <raylandermarques24@gmail.com>
2022-07-11 10:00:47 -0300

    back end com cadastro login e retorno dos usuários

---
+++ chatback/src/test/java/web/trabalho/chatback/ChatbackApplicationTests.java
@@ -0,0 +1,13 @@
+package web.trabalho.chatback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
